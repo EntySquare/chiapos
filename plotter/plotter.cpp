333c855ef47e770796dd3a25f873299acba27c64
luo <506765498@qq.com>
2021-05-15 20:53:12 +0800

    modify plotter.cpp

--- plotter/plotter.cpp
+++ plotter/plotter.cpp
@@ -70,7 +70,7 @@ int main(int argc, char *argv[]) try {
         .show_positional_help();
 
     // Default values
-    uint8_t k = 20;
+    uint8_t k = 32;
     uint32_t num_buckets = 0;
     uint32_t num_stripes = 0;
     uint8_t num_threads = 0;
@@ -151,135 +151,6 @@ int main(int argc, char *argv[]) try {
                 num_threads,
                 nobitfield,
                 show_progress);
-    } else if (operation == "prove") {
-        if (argc < 3) {
-            HelpAndQuit(options);
-        }
-        cout << "Proving using filename=" << filename << " challenge=" << argv[2] << endl
-             << endl;
-        string challenge = Strip0x(argv[2]);
-        if (challenge.size() != 64) {
-            cout << "Invalid challenge, should be 32 bytes" << endl;
-            exit(1);
-        }
-        uint8_t challenge_bytes[32];
-        HexToBytes(challenge, challenge_bytes);
-
-        DiskProver prover(filename);
-        try {
-            vector<LargeBits> qualities = prover.GetQualitiesForChallenge(challenge_bytes);
-            for (uint32_t i = 0; i < qualities.size(); i++) {
-                k = prover.GetSize();
-                uint8_t *proof_data = new uint8_t[8 * k];
-                LargeBits proof = prover.GetFullProof(challenge_bytes, i);
-                proof.ToBytes(proof_data);
-                cout << "Proof: 0x" << Util::HexStr(proof_data, k * 8) << endl;
-                delete[] proof_data;
-            }
-            if (qualities.empty()) {
-                cout << "No proofs found." << endl;
-                exit(1);
-            }
-        } catch (const std::exception& ex) {
-            std::cout << "Error proving. " << ex.what() << std::endl;
-            exit(1);
-        } catch (...) {
-            std::cout << "Error proving. " << std::endl;
-            exit(1);
-        }
-    } else if (operation == "verify") {
-        if (argc < 4) {
-            HelpAndQuit(options);
-        }
-        Verifier verifier = Verifier();
-
-        id = Strip0x(id);
-        string proof = Strip0x(argv[2]);
-        string challenge = Strip0x(argv[3]);
-        if (id.size() != 64) {
-            cout << "Invalid ID, should be 32 bytes" << endl;
-            exit(1);
-        }
-        if (challenge.size() != 64) {
-            cout << "Invalid challenge, should be 32 bytes" << endl;
-            exit(1);
-        }
-        if (proof.size() % 16) {
-            cout << "Invalid proof, should be a multiple of 8 bytes" << endl;
-            exit(1);
-        }
-        k = proof.size() / 16;
-        cout << "Verifying proof=" << argv[2] << " for challenge=" << argv[3]
-             << " and k=" << static_cast<int>(k) << endl
-             << endl;
-        uint8_t id_bytes[32];
-        uint8_t challenge_bytes[32];
-        uint8_t *proof_bytes = new uint8_t[proof.size() / 2];
-        HexToBytes(id, id_bytes);
-        HexToBytes(challenge, challenge_bytes);
-        HexToBytes(proof, proof_bytes);
-
-        LargeBits quality =
-            verifier.ValidateProof(id_bytes, k, challenge_bytes, proof_bytes, k * 8);
-        if (quality.GetSize() == 256) {
-            cout << "Proof verification suceeded. Quality: " << quality << endl;
-        } else {
-            cout << "Proof verification failed." << endl;
-            exit(1);
-        }
-        delete[] proof_bytes;
-    } else if (operation == "check") {
-        uint32_t iterations = 1000;
-        if (argc == 3) {
-            iterations = std::stoi(argv[2]);
-        }
-
-        DiskProver prover(filename);
-        Verifier verifier = Verifier();
-
-        uint32_t success = 0;
-        uint8_t id_bytes[32];
-        prover.GetId(id_bytes);
-        k = prover.GetSize();
-
-        for (uint32_t num = 0; num < iterations; num++) {
-            vector<unsigned char> hash_input = intToBytes(num, 4);
-            hash_input.insert(hash_input.end(), &id_bytes[0], &id_bytes[32]);
-
-            vector<unsigned char> hash(picosha2::k_digest_size);
-            picosha2::hash256(hash_input.begin(), hash_input.end(), hash.begin(), hash.end());
-
-            try {
-                vector<LargeBits> qualities = prover.GetQualitiesForChallenge(hash.data());
-
-                for (uint32_t i = 0; i < qualities.size(); i++) {
-                    LargeBits proof = prover.GetFullProof(hash.data(), i);
-                    uint8_t *proof_data = new uint8_t[proof.GetSize() / 8];
-                    proof.ToBytes(proof_data);
-                    cout << "i: " << num << std::endl;
-                    cout << "challenge: 0x" << Util::HexStr(hash.data(), 256 / 8) << endl;
-                    cout << "proof: 0x" << Util::HexStr(proof_data, k * 8) << endl;
-                    LargeBits quality =
-                        verifier.ValidateProof(id_bytes, k, hash.data(), proof_data, k * 8);
-                    if (quality.GetSize() == 256 && quality == qualities[i]) {
-                        cout << "quality: " << quality << endl;
-                        cout << "Proof verification suceeded. k = " << static_cast<int>(k) << endl;
-                        success++;
-                    } else {
-                        cout << "Proof verification failed." << endl;
-                    }
-                    delete[] proof_data;
-                }
-            } catch (const std::exception& error) {
-                cout << "Threw: " << error.what() << endl;
-                continue;
-            }
-        }
-        std::cout << "Total success: " << success << "/" << iterations << ", "
-                  << (success * 100 / static_cast<double>(iterations)) << "%." << std::endl;
-        if (show_progress) { progress(4, 1, 1); }
-    } else {
-        cout << "Invalid operation. Use create/prove/verify/check" << endl;
     }
     return 0;
 } catch (const cxxopts::OptionException &e) {
