839c88c47292285989ebfc1300e5b547aa67f84c
luo <506765498@qq.com>
2021-05-19 01:59:39 +0800

    modify plotter.cpp

--- plotter/plotter.cpp
+++ plotter/plotter.cpp
@@ -177,7 +177,7 @@ try {
         derivePath[2] = 3;
         derivePath[3] = 0;
         for (int i = 0; i < 4; i++) {
-            sk = bls::AugSchemeMPL().DeriveChildSk(LocalSk, i);
+            LocalSk = bls::AugSchemeMPL().DeriveChildSk(LocalSk, i);
         }
         bls::G1Element localSk = LocalSk.GetG1Element();
         std::vector<uint8_t> farmerArray(48);
@@ -185,7 +185,7 @@ try {
         bls::G1Element farmerPublicKey;
         farmerPublicKey = bls::G1Element::FromByteVector(farmerArray);
         std::vector<uint8_t> poolArray(48);
-        HexToBytes(farmer_public_key, poolArray.data());
+        HexToBytes(pool_public_key, poolArray.data());
         bls::G1Element poolPublicKey;
         poolPublicKey = bls::G1Element::FromByteVector(poolArray);
         bls::G1Element plotPublicKey = localSk + farmerPublicKey;
@@ -193,8 +193,10 @@ try {
         msg_id.insert(msg_id.end(), poolArray.begin(), poolArray.end());
         cout << "id_size1 >>>>>> " << static_cast<int>(msg_id.size()) << endl;
         // TODO fix insert plot_public_key
-        msg_id.insert(
-            msg_id.end(), plotPublicKey.Serialize().begin(), plotPublicKey.Serialize().end());
+        cout << "plot_public_key_size >>>>>> " << static_cast<int>(sizeof(plotPublicKey)) << endl;
+        vector<uint8_t> PlotPKBuffer(sizeof(plotPublicKey));
+        std::memcpy(PlotPKBuffer.data(), plotPublicKey.Serialize().data(), sizeof(plotPublicKey));
+        msg_id.insert(msg_id.end(), PlotPKBuffer.begin(), PlotPKBuffer.end());
         cout << "id_size2 >>>>>> " << static_cast<int>(msg_id.size()) << endl;
         vector<uint8_t> msg_memo;
         msg_memo.insert(msg_memo.end(), poolArray.begin(), poolArray.end());
@@ -202,7 +204,11 @@ try {
         msg_memo.insert(msg_memo.end(), farmerArray.begin(), farmerArray.end());
         cout << "memo_size2 >>>>>> " << static_cast<int>(msg_memo.size()) << endl;
         // TODO fix insert sk
-        msg_memo.insert(msg_memo.end(), sk.Serialize().begin(), sk.Serialize().end());
+        uint8_t skBuffer[32];
+        sk.Serialize(skBuffer);
+        vector<uint8_t> SkBuffer(32);
+        std::memcpy(SkBuffer.data(), skBuffer, SkBuffer.size());
+        msg_memo.insert(msg_memo.end(), SkBuffer.begin(), SkBuffer.end());
         cout << "memo_size3 >>>>>> " << static_cast<int>(msg_memo.size()) << endl;
         vector<uint8_t> id_bytes(32);
         vector<uint8_t> memo_bytes(static_cast<int>(msg_memo.size()));
@@ -235,8 +241,9 @@ try {
         cout << "tempdir=" << tempdir << ";tempdir2=" << tempdir2 << ";finaldir=" << finaldir
              << ";k=" << static_cast<int>(k) << ";memo=" << memo << ";id=" << id
              << ";buffmegabytes=" << static_cast<int>(buffmegabytes)
-             << ";num_buckets=" << static_cast<int>(num_buckets) << ";num_stripes"
-             << static_cast<int>(num_stripes) << ";num_threads=" << static_cast<int>(num_threads)
+             << ";num_buckets=" << static_cast<int>(num_buckets)
+             << ";num_stripes=" << static_cast<int>(num_stripes)
+             << ";num_threads=" << static_cast<int>(num_threads)
              << ";nobitfield=" << static_cast<bool>(nobitfield)
              << ";show_progress=" << static_cast<bool>(show_progress) << ";filename=" << filename
              << endl;
